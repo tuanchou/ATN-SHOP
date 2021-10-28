519e9a523cc44f447c877463289d6e491b8fc3cf
tuanthai <87470741+tuanthai@users.noreply.github.com>
2021-10-28 11:27:30 +0700

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-//const url = "mongodb://localhost:27017/EmployeeDB";
-const url = "mongodb+srv://tmtuan:**************@cluster0.zfovn.mongodb.net/test"
+const url = "mongodb://localhost:27017/EmployeeDB";
+//const url = "mongodb+srv://tmtuan:**************@cluster0.zfovn.mongodb.net/test"
 mongoose.connect(url,{useNewUrlParser:true},(err) => {
     if(!err){ console.log("MongoDB Connection Succeeded");}
     else{
