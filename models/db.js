13cce5e49492f38387d5efc7ce9be769fa98dfc3
tuanchou <tuantran13005@gmail.com>
2022-10-17 19:00:19 +0700

    npm

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //const url = "mongodb://localhost:27017/EmployeeDB";
 //const url = "mongodb+srv://tmtuan:**************@cluster0.zfovn.mongodb.net/test"
-const url = "mongodb+srv://tmtuan:Zdub8FnkJh9cTrsm@cluster0.e6jqisa.mongodb.net/?retryWrites=true&w=majority"
+const url = "mongodb+srv://tuanchou1305:tuanchou1305@cluster0.do6tx1l.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(url,{useNewUrlParser:true},(err) => {
     if(!err){ console.log("MongoDB Connection Succeeded");}
     else{
